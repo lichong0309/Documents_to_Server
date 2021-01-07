ef2f39608701649144e95e764114f814fd53be21
lichong0309 <chongli0309@gmail.com>
2021-01-07 16:33:09 +0900

    update

--- SGX-demo/enclave-demo/Enclave/enclave.c
+++ SGX-demo/enclave-demo/Enclave/enclave.c
@@ -5,7 +5,7 @@ char * secret = "I am the SGX secret\n";
 char buff[100]; // used to be copied outside of Enclave
 
 void e_call_print_secret() {
-    char * secret = "modify";
+    char * secret = "modify\n";
     snprintf(buff,100,"%s",secret);
     // printf("%s\n",secret);
     ocall_print_string(buff);
