1ba381e0c86219e98d0b0b3b5ad18efa062cb39d
lichong0309 <chongli0309@gmail.com>
2021-02-16 10:22:17 +0900

    update

--- plinius/App/App.cpp
+++ plinius/App/App.cpp
@@ -67,7 +67,7 @@ comm_info *comm_out;
 
 
 // darknet框架神经网络的配置文件mnist.cfg路径
-#define MNIST_CFG "./App/dnet-out/cfg/big/cfg50.cfg"
+#define MNIST_CFG "./App/dnet-out/cfg/big/cfg10.cfg"
 
 
 /* For benchmarking */
