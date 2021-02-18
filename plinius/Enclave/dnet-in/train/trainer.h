df66ad55b785a001e3cef2fc192bac6985572d88
lichong0309 <chongli0309@gmail.com>
2021-02-18 17:21:21 +0900

    update

--- plinius/Enclave/dnet-in/train/trainer.h
+++ plinius/Enclave/dnet-in/train/trainer.h
@@ -8,7 +8,7 @@
 void train_cifar(list *sections, data *training_data, int pmem);
 void train_mnist(list *sections, data *training_data, int pmem);
 void test_cfiar(list *sections, data *test_data, int pmem);
-void test_mnist(list *sections, data *test_data, int pmem);
+float test_mnist(list *sections, data *test_data, int pmem);
 void classify_tiny(list *sections, list *labels, image *im, int top);
 
 #if defined(__cplusplus)
